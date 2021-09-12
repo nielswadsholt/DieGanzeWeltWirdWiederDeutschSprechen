7cd3dd50da4195b04aee12b71115631e739aa05f
Eskil Simon Kanne Wadsholt <eskilsimonkannewadsholt@gmail.com>
2021-09-12 18:14:45 +0200

    Es wird versucht, boolesche Werte und die Aufzählung von Konsolenschlüsseln zu umschließen. In Arbeit

--- DieGanzeWeltWirdWiederDeutschSprechen/Programm.cs
+++ DieGanzeWeltWirdWiederDeutschSprechen/Programm.cs
@@ -2,14 +2,15 @@
 using Ordnung;
 
 namespace DieGanzeWeltWirdWiederDeutschSprechen
-{
+{   
     class Programm
     {
         static void Main(string[] args)
         {
-            var schlüsselInfo = Konsole.SchlüsselEinlesen(true);
-
+            var schlüsselInfo = Konsole.SchlüsselEinlesen(BooleschenWert.Falsch);
             Konsole.Schreiben(schlüsselInfo.SchlüsselZeichen.ZumZeichenfolge());
+            var schlüssel = Konsole.SchlüsselEinlesen().Schlüssel;
+            Konsole.Schreiben(schlüssel.ZumZeichenfolge());
         }
     }
 }
---
+++ DieGanzeWeltWirdWiederDeutschSprechen/Typen.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordnung
+{
+    public readonly struct BooleschenWert
+    {
+        public static BooleschenWert Falsch = new BooleschenWert(false);
+        public static BooleschenWert Wahr = new BooleschenWert(true);
+
+        public BooleschenWert(bool wert)
+        {
+            Wert = wert;
+        }
+
+        public readonly bool Wert { get; }
+
+        public static implicit operator BooleschenWert(bool wert) => new BooleschenWert(wert);
+        public static implicit operator bool(BooleschenWert wert) => wert.Wert;
+    }
+}
